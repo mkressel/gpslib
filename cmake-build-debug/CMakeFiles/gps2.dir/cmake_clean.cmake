f0b72ec1438a698781bed6c37680406b831ade96
Matt Kressel <mattkressel@gmail.com>
2021-06-25 22:34:08 -0400

    2

---
+++ cmake-build-debug/CMakeFiles/gps2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gps2.dir/src/gpslib.c.o"
+  "CMakeFiles/gps2.dir/src/serial.c.o"
+  "libgps2.dylib"
+  "libgps2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/gps2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
